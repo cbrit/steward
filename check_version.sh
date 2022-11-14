18f5e77386dfc70534dfe1e8ed16b80f2089b4e9
Collin Brittain <collinbrittain@gmail.com>
2022-11-14 11:14:39 -0600

    uggggghhhhhhhh

--- check_version.sh
+++ check_version.sh
@@ -4,9 +4,9 @@ set -e
 
 BINARY_VERSION_STRING=$(cat steward/Cargo.toml | grep "^version.*" | head -1)
 # above output is 'version = "x.x.x"' so we trim the leading and trailing characters
-BINARY_VERSION=echo ${BINARY_VERSION_STRING:11:-1}
+BINARY_VERSION=${BINARY_VERSION_STRING:11:-1}
 # trim the leading 'v' from the tag
-RELEASE_VERSION=echo ${GITHUB_REF_NAME: 1}
+RELEASE_VERSION=${GITHUB_REF_NAME: 1}
 
 echo "Release version: $RELEASE_VERSION"
 echo "Binary version: $BINARY_VERSION"
