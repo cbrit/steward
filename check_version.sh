fdee5961239d8bc94662a905ac852cdb0ad2ab8e
Collin Brittain <collinbrittain@gmail.com>
2022-11-14 11:12:21 -0600

    fix spacing

--- check_version.sh
+++ check_version.sh
@@ -2,11 +2,11 @@
 
 set -e
 
-BINARY_VERSION_STRING = $(cat steward/Cargo.toml | grep "^version.*" | head -1)
+BINARY_VERSION_STRING=$(cat steward/Cargo.toml | grep "^version.*" | head -1)
 # above output is 'version = "x.x.x"' so we trim the leading and trailing characters
-BINARY_VERSION = echo ${BINARY_VERSION_STRING:11:-1}
+BINARY_VERSION=echo ${BINARY_VERSION_STRING:11:-1}
 # trim the leading 'v' from the tag
-RELEASE_VERSION = echo ${GITHUB_REF_NAME: 1}
+RELEASE_VERSION=echo ${GITHUB_REF_NAME: 1}
 
 echo "Release version: $RELEASE_VERSION"
 echo "Binary version: $BINARY_VERSION"
